5cb43d67d101f35e7567b51bc25fd9ec1ded5207
Nikita Kotvitskiy <nikita.kotvitski@gmail.com>
2023-12-08 16:08:18 +0100

    Enemies added
    Enemy can fly and shoot. They don't react to player's bullets. Player don't react to enemy's bullets

--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var rock_scene : PackedScene
+@export var enemy_scene : PackedScene
 
 var screensize = Vector2.ZERO
 
@@ -55,6 +56,7 @@ func new_level():
 	$HUD.show_message("Wave %s" % level)
 	for i in level:
 		spawn_rock(3)
+	$EnemyTimer.start(randf_range(5, 10))
 
 func game_over():
 	playing = false
@@ -72,3 +74,10 @@ func _input(event):
 		else:
 			message.text = ""
 			message.hide()
+
+
+func _on_enemy_timer_timeout():
+	var e = enemy_scene.instantiate()
+	add_child(e)
+	e.target = $Player
+	$EnemyTimer.start(randf_range(20, 40))
